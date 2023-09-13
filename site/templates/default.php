cc38659639b6f39b7eb5756f9dc823da665e4b87
Taichi Aritomo <taichiaritomo@gmail.com>
2023-09-13 15:30:39 -0400

    Set up CSS and Tailwind

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,7 @@
-<h1><?= $page->title() ?></h1>
+<?php snippet('header') ?>
+<div class="w-full min-h-full bg-yellow">
+  <header class="w-full pt-6">
+    <h1 class="uppercase text-center"><?= $page->title() ?></h1>
+  </header>
+</div>
+<?php snippet('footer') ?>
