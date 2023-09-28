baa3b3f7c29c18d41da9e24ba1caccea3854f2fa
Taichi Aritomo <taichiaritomo@gmail.com>
2023-09-28 14:59:09 -0400

    Styling the home page with tailwind

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,17 +1,22 @@
 <?php snippet('header') ?>
 
-<div class="w-full min-h-full bg-yellow">
+<div class="w-full min-h-screen bg-yellow flex flex-col justify-between items-center">
+    <div class="grow-0 shrink-0">
+        <?php snippet('title') ?>
+    </div>
 
-  <?= $site->splash_text()->kirbytext() ?>
+    <div class="w-[519px] grow shrink-0 flex justify-center items-center">
+        <div>
+            <?= $site->welcome_text()->kirbytext() ?>
+        </div>
+    </div>
 
-  <?= $site->welcome_text()->kirbytext() ?>
-
-    <menu>
-        <li><a class="selfies" href="selfies">Selfies</a></li>
+    <menu class="flex gap-2 grow-0 shrink-0 mb-4">
+        <li class="ats-button"><a class="selfies" href="selfies">Selfies</a></li>
         <?php foreach ($site->pages()->listed()->filterBy('template', 'default') as $menu_item): ?>
-            <li><a class="<?php e($page->slug() == $menu_item->slug(), 'current') ?>" href="<?= $menu_item->url() ?>"><?= $menu_item->title() ?></a></li>
+            <li class="ats-button"><a class="<?php e($page->slug() == $menu_item->slug(), 'current') ?>" href="<?= $menu_item->url() ?>"><?= $menu_item->title() ?></a></li>
         <?php endforeach ?>
-        <li><a class="essays" href="essays">Essays</a></li>
+        <li class="ats-button"><a class="essays" href="essays">Essays</a></li>
     </menu>
 
 </div>
