f92a56f7e875124f0777b4c4b92cc22762e58fb0
Laurel Schwulst <laurel.schwulst@gmail.com>
2023-10-02 13:31:13 -0700

    stars

--- site/snippets/essay-title.php
+++ site/snippets/essay-title.php
@@ -3,6 +3,8 @@
 </h2>
 
 <div class="text-center">
-    <div class="mt-3 mb-3">*</div>
+    <div class="mt-3 mb-3 text-center">
+        <img src="<?= $site->url() ?>/assets/images/star01.svg" width="25" style="display: inline-block !important;">
+    </div>
     <i><?= $page->author() ?></i>
 </div>
